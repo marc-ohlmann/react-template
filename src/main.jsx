cd8e420c75d7a316a00f366aad54705214bb8542
Marc Ohlmann <marc.ohlmann25@gmail.com>
2025-06-09 11:44:06 -0700

    added pages folder and renamed App to HomePage

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css'
-import App from './App.jsx'
-import ErrorPage from './ErrorPage.jsx'
+import HomePage from './pages/HomePage.jsx'
+import ErrorPage from './pages/ErrorPage.jsx'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <HomePage />,
     errorElement: <ErrorPage />,
   },
 ]);
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import './HomePage.css'
+
+function HomePage() {
+
+  return (
+    <>
+      <h1>Hello World!</h1>
+    </>
+  )
+}
+
+export default HomePage
