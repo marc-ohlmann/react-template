f3f7b7d5f0fb9e23a94c8396ec31bce857b5079f
Marc Ohlmann <marc.ohlmann25@gmail.com>
2025-06-09 11:31:19 -0700

    added react router and an error page for invalid urls

---
+++ src/ErrorPage.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+const ErrorPage = () => {
+  return (
+    <div>
+      <h2>Oh no, this route doesn't exist!</h2>
+      <Link to="/">
+        Return to home page
+      </Link>
+    </div>
+  );
+};
+
+export default ErrorPage;
